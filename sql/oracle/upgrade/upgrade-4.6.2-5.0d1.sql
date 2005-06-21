c59ae84bbac16b8df263962e13d1e859475cf4b3
Andrew Grumet <aegrumet@alum.mit.edu>
2005-06-21 19:30:48 +0000

    Rearranging definition order to fix compilation bugs.  Now the all of the package definitions come first, the bodies second.

--- sql/oracle/upgrade/upgrade-4.6.2-5.0d1.sql
+++ sql/oracle/upgrade/upgrade-4.6.2-5.0d1.sql
@@ -27,51 +27,6 @@ end acs_mail_queue_message;
 /
 show errors
 
-create or replace package body acs_mail_queue_message
-as
-
- function new (
-  mail_link_id    in acs_mail_links.mail_link_id%TYPE default null,
-  body_id         in acs_mail_bodies.body_id%TYPE,
-
-  context_id      in acs_objects.context_id%TYPE    default null,
-  creation_date   in acs_objects.creation_date%TYPE default sysdate,
-  creation_user   in acs_objects.creation_user%TYPE default null,
-  creation_ip     in acs_objects.creation_ip%TYPE   default null,
-  object_type     in acs_objects.object_type%TYPE   default 'acs_mail_link'
- ) return acs_objects.object_id%TYPE
- is
-     v_object_id acs_objects.object_id%TYPE;
- begin
-     v_object_id := acs_mail_link.new (
-         mail_link_id => mail_link_id,
-		 body_id => body_id,		      
-         context_id => context_id,
-         creation_date => creation_date,
-         creation_user => creation_user,
-         creation_ip => creation_ip,
-         object_type => object_type
-     );
-     insert into acs_mail_queue_messages ( message_id )
-         values ( v_object_id );
-     return v_object_id;
- end;
-
- procedure del (
-  message_id in acs_mail_links.mail_link_id%TYPE
- )
- is
- begin
-     delete from acs_mail_queue_messages
-         where message_id = acs_mail_queue_message.del.message_id;
-     acs_mail_link.del(message_id);
- end;
-
-end acs_mail_queue_message;
-/
-show errors
-
-
 --
 -- packages/acs-mail/sql/acs-mail-create-packages.sql
 --
@@ -218,6 +173,50 @@ end acs_mail_link;
 show errors
 
 -- Package Implementations ---------------------------------------------
+
+create or replace package body acs_mail_queue_message
+as
+
+ function new (
+  mail_link_id    in acs_mail_links.mail_link_id%TYPE default null,
+  body_id         in acs_mail_bodies.body_id%TYPE,
+
+  context_id      in acs_objects.context_id%TYPE    default null,
+  creation_date   in acs_objects.creation_date%TYPE default sysdate,
+  creation_user   in acs_objects.creation_user%TYPE default null,
+  creation_ip     in acs_objects.creation_ip%TYPE   default null,
+  object_type     in acs_objects.object_type%TYPE   default 'acs_mail_link'
+ ) return acs_objects.object_id%TYPE
+ is
+     v_object_id acs_objects.object_id%TYPE;
+ begin
+     v_object_id := acs_mail_link.new (
+         mail_link_id => mail_link_id,
+		 body_id => body_id,		      
+         context_id => context_id,
+         creation_date => creation_date,
+         creation_user => creation_user,
+         creation_ip => creation_ip,
+         object_type => object_type
+     );
+     insert into acs_mail_queue_messages ( message_id )
+         values ( v_object_id );
+     return v_object_id;
+ end;
+
+ procedure del (
+  message_id in acs_mail_links.mail_link_id%TYPE
+ )
+ is
+ begin
+     delete from acs_mail_queue_messages
+         where message_id = acs_mail_queue_message.del.message_id;
+     acs_mail_link.del(message_id);
+ end;
+
+end acs_mail_queue_message;
+/
+show errors
 
 create or replace package body acs_mail_gc_object
 as
